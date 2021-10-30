9bc55cc1a6a88c7361644bfd34ff1d7538ab0832
Quentinx6 <82029618+Quentinx6@users.noreply.github.com>
2021-10-30 14:33:30 +0200

    completed 1

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("http://api.countrylayer.com/v2/all?access_key=fc91c1fe5b588cfc0ea5a55073d0761f")
+    // fetch("http://api.countrylayer.com/v2/all?access_key=fc91c1fe5b588cfc0ea5a55073d0761f")
+    fetch("https://restcountries.com/v2/all")
     
         .then((res) => res.json())
         .then((data)=>{
@@ -8,12 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
             let namePays = document.getElementsByClassName("namePays")[0];
             let populationPays = document.getElementsByClassName("populationPays")[0];
             let capitalPays = document.getElementsByClassName("capitalPays")[0];
+            let regionPays = document.getElementsByClassName("regionPays")[0];
+            let imgFlag = document.getElementsByClassName("imgFlag")[0];
             let options = document.getElementsByTagName("option");
-            select.addEventListener("change", () => {
-                let code3 = select.value;
-                populationPays.innerHTML = data[code3]["capital"];
-                console.log(data[code3]);
-            });
+            let nativename = document.getElementsByClassName("nativename")[0];
+            let nameFrench = document.getElementsByClassName("nameFrench")[0];
+            let frontiere = document.getElementsByClassName("frontiere")[0];
             let name = "";
             let tableauName = [];
             for(let i = 0 ; i < data.length ; i++ ){
@@ -24,16 +25,31 @@ document.addEventListener("DOMContentLoaded", () => {
                 option.setAttribute("value", code)
                 option.innerHTML += name;
             }
-            // function leChoix(e){
-            //     let choice = select.selectIndex;
-            //     let value = select.options[e].value;
-            //     console.log(value);
+            select.addEventListener("change", () => {
+                frontiere.innerHTML = " ";
+                let code3 = select.value;
+                let popu = data[code3]["population"];
+                let front = data[code3]["borders"];
+                populationPays.innerHTML = popu.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " habitants.";
+                capitalPays.innerHTML = data[code3]["capital"];
+                regionPays.innerHTML = data[code3]["region"];
+                imgFlag.setAttribute("src", data[code3]["flags"]["svg"]);
+                nativename.innerHTML = data[code3]["nativeName"];
+                nameFrench.innerHTML = data[code3]["translations"]["fr"];
+                if(front === undefined){
+                    frontiere.innerHTML = "Aucun pays."
+                }else{
+                    for(let i = 0; i < front.length; i++){
+                        frontiere.innerHTML += front[i];
+                        if(i < front.length-1){
+                            frontiere.innerHTML += ", ";
+                        }
+    
+                    }
+                }
 
-            // }
-            console.log(select);
-            console.log(namePays);
-            console.log(populationPays)
-            console.log(capitalPays)
+                console.log(data[code3]);
+            });
         })
         .catch((err) => console.log(err));
 })
